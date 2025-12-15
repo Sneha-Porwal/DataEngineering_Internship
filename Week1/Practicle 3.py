4e4c030fa6847adaa02430d66c6831c40ccf3a17
Sneha-Porwal <142002344+Sneha-Porwal@users.noreply.github.com>
2025-12-15 16:48:53 +0530

    commit

---
+++ Week1/Practicle 3.py
@@ -0,0 +1,22 @@
+'''List:
+When you check an element in a list, Python looks at each item one by one until it finds the value.
+If the list is large, this takes more time.'''
+
+numbers_list = [10, 20, 30, 40, 50]
+
+if 40 in numbers_list:
+    print("Element found in list")
+
+
+#Time Complexity:O(n) (Linear time)
+
+""" Set:
+A set uses hashing to find elements directly."""
+
+numbers_set = {10, 20, 30, 40, 50}
+
+if 40 in numbers_set:
+    print("Element found in set")
+
+
+#Time Complexity: O(1) (Constant time, average case)
