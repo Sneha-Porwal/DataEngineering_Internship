3020d37c4de8aa3f83706913a112e6974c653960
Sneha-Porwal <142002344+Sneha-Porwal@users.noreply.github.com>
2025-12-24 11:29:53 +0530

    commit

--- Week1/Day4/Practical3.py
+++ Week1/Day4/Practical3.py
@@ -1,13 +1,29 @@
-'''Rate limiting is used by APIs to control how many requests a client can make within a given time period, helping prevent server overload and abuse.
-Rate limiting is used by APIs to control how many requests a client can make within a given time period, helping prevent server overload and abuse.
+'''Rate limiting is used by APIs to control how many requests a client can make within a given time period, helping prevent server overloading.
 
-Ways to Handle Rate Limiting
+How to handle it in Python:
 
-Check Rate-Limit Headers
-Most APIs include rate-limit information in HTTP response headers, such as:
+1.Check the response status code after making a request
 
-1. X-RateLimit-Limit – total allowed requests
+2.If the API returns 429 Too Many Requests, it means you sent too many requests
 
-X-RateLimit-Remaining – requests left
+3.Pause your program for some time using time.sleep()
 
-3. X-RateLimit-Reset – time when the limit resets'''
+4.Try the request again after waiting
+
+5.Use authentication (API key/token) to get higher limits
+
+'''
+
+import requests
+import time
+
+url = "https://restcountries.com/v3.1/all"
+response = requests.get(url)
+
+if response.status_code == 429:
+    print("Too many requests. Waiting...")
+    time.sleep(60)   # wait for 1 minute
+    response = requests.get(url)
+else:
+    print(response.json())
+
